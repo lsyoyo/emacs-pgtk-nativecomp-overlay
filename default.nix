0bcc0e90559729b493b61aacc15775eee2553bd4
Michael Lingelbach <m.j.lbach@gmail.com>
2020-11-08 22:30:48 -0800

    Fix only being able to run via command line on macos

--- default.nix
+++ default.nix
@@ -62,6 +62,19 @@ let
               --replace '(emacs-repository-get-branch)' '"master"'
             '';
 
+            configureFlags = old.configureFlags ++ pkgs.stdenv.lib.optional pkgs.stdenv.isDarwin [ "--with-ns" ];
+
+            postInstall = old.postInstall + pkgs.stdenv.lib.optionalString pkgs.stdenv.isDarwin ''
+              ln -snf $out/lib/emacs/28.0.50/native-lisp $out/native-lisp
+              ln -snf $out/lib/emacs/28.0.50/native-lisp $out/Applications/Emacs.app/Contents/native-lisp
+              cat <<EOF> $out/bin/run-emacs.sh
+              #!/usr/bin/env bash
+              set -e
+              exec $out/bin/emacs-28.0.50 "\$@"
+              EOF
+              chmod a+x $out/bin/run-emacs.sh
+              ln -snf ./run-emacs.sh $out/bin/emacs
+            '';
           }
         )
       )
